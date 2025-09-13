f1b189e21a2059301e9ed7eb3f3d615cdaa0a72a
markjorias <maorias@my.cspc.edu.ph>
2025-09-13 23:59:06 +0800

    Update Week 3 labs: Flet User Login Application
    Changelog:
    - Updated main.py user interface.

--- week3_labs/src/main.py
+++ week3_labs/src/main.py
@@ -11,6 +11,7 @@ def main(page: ft.Page):
     page.window.frameless = True
     page.window.height = 350
     page.window.width = 400
+    page.padding = 30 # PADDING AROUND THE CONTENT SO BUTTON DOESN'T TOUCH THE EDGES
     
     # Page Layout Configuration
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
@@ -19,44 +20,95 @@ def main(page: ft.Page):
     # Page Background Color
     page.bgcolor = ft.Colors.AMBER_ACCENT
     
-    # counter = ft.Text("0", size=50, data=0)
-
-    # def increment_click(e):
-    #     counter.data += 1
-    #     counter.value = str(counter.data)
-    #     counter.update()
-
-    # page.floating_action_button = ft.FloatingActionButton(
-    #     icon=ft.Icons.ADD, on_click=increment_click
-    # )
+    # User Interface Controls
+    
+    # Login Title
+    login_title = ft.Text(
+        "User Login",
+        text_align=ft.TextAlign.CENTER,
+        size=20,
+        weight=ft.FontWeight.BOLD,
+        font_family="Arial",
+        color=ft.Colors.BLACK,
+    )
+    
+    # Username Input Field
+    username_field = ft.TextField(
+        label="User name",
+        label_style=ft.TextStyle(color=ft.Colors.BLACK54),
+        hint_text="Enter your username",
+        hint_style=ft.TextStyle(color=ft.Colors.BLACK54),
+        helper_text="This is your unique identifier",
+        helper_style=ft.TextStyle(color=ft.Colors.BLACK54),
+        width=300,
+        bgcolor=ft.Colors.LIGHT_BLUE_ACCENT,
+        color=ft.Colors.BLACK87,
+        border_color=ft.Colors.BLACK54,
+    )
+    
+    # Password Input Field
+    password_field = ft.TextField(
+        label="Password",
+        label_style=ft.TextStyle(color=ft.Colors.BLACK54),
+        hint_text="Enter your password",
+        hint_style=ft.TextStyle(color=ft.Colors.BLACK54),
+        helper_text="This is your secret key",
+        helper_style=ft.TextStyle(color=ft.Colors.BLACK54),
+        width=300,
+        password=True,
+        can_reveal_password=True,
+        color=ft.Colors.BLACK87,
+        bgcolor=ft.Colors.LIGHT_BLUE_ACCENT,
+        border_color=ft.Colors.BLACK54,
+    )
+    
+    login_button = ft.ElevatedButton(
+        "Login",
+        on_click = lambda e: print("Login Clicked"),
+        width = 100,
+        icon = ft.Icons.LOGIN,
+        style=ft.ButtonStyle(
+            color=ft.Colors.BLUE_GREY, 
+            bgcolor=ft.Colors.WHITE, 
+            text_style=ft.TextStyle(
+                color=ft.Colors.BLACK87,
+                weight=ft.FontWeight.BOLD,)
+        )
+    )
     
     page.add(
         ft.Container(
-            content = ft.Column(
-                [
-                    ft.Text(
-                        "User Login",
-                        font_family = "Google Sans Flex",
-                        size = 24,
-                        weight = ft.FontWeight.BOLD,
-                        color = ft.Colors.BLACK
-                    )
-                ],
-                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                spacing=10,
+            content=ft.Column(
+            [
+                login_title,
+                ft.Row(
+                    [
+                        ft.Icon(name=ft.Icons.PERSON, color = "#43474e"),
+                        username_field,
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                ),
+                
+                ft.Row(
+                    [
+                        ft.Icon(name=ft.Icons.PASSWORD, color = "#43474e"),
+                        password_field
+                    ],
+                    alignment=ft.MainAxisAlignment.CENTER,
+                    vertical_alignment=ft.CrossAxisAlignment.CENTER,
+                )
+            ],
+            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            )
         )
     )
-    )
     
-    # page.add(
-    #     ft.SafeArea(
-    #         ft.Container(
-    #             counter,
-    #             alignment=ft.alignment.center,
-    #         ),
-    #         expand=True,
-    #     )
-    # )
-
+    page.add(
+        ft.Container(
+            content = login_button,
+            alignment=ft.alignment.bottom_right,
+        )
+    )
 
 ft.app(main)
